a139e8a9d33fbb2f6e41b4a5863cdf517aa5a5a6
NavarroEmiliano <emilianonavarro1993@gmail.com>
2023-05-06 20:26:55 -0300

    Segundo commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const ARRMOVIES = [
+  "Interestelar",
+  "John Wick 4",
+  "Green Book",
+  "El padrino",
+  "Drive to survive",
+  "Inferno",
+  "Dia de la independencia",
+  "Titanic"
+];
+
+
+
+const d = document;
+
