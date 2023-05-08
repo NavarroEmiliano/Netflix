043863230b7957e5c5d48dda31c2037d48e1da66
NavarroEmiliano <emilianonavarro1993@gmail.com>
2023-05-08 01:11:37 -0300

    Casi terminado

--- js/index.js
+++ js/index.js
@@ -6,10 +6,36 @@ const ARRMOVIES = [
   "Drive to survive",
   "Inferno",
   "Dia de la independencia",
-  "Titanic"
+  "Titanic",
 ];
 
-
-
 const d = document;
+const $lupa = d.getElementById("searchIcon");
+const $lupaInput = d.getElementById("searchInput");
+const $searchListContainer = d.getElementById("searchListContainer");
+const $listaDePeliculas = d.getElementById("searchList");
 
+$lupa.addEventListener("click", (e) => {
+  if ($lupa.classList.toggle("activated")) {
+    $lupaInput.style.visibility = "visible";
+    $lupaInput.style.opacity = 1;
+    $searchListContainer.style.display = "block";
+
+    $lupaInput.addEventListener("input", (e) => {
+      let $value = e.target.value;
+      $listaDePeliculas.innerHTML = "";
+      for (let i = 0; i < ARRMOVIES.length; i++) {
+        const MOVIE = ARRMOVIES[i];
+        if (MOVIE.toLocaleLowerCase().includes($value.toLocaleLowerCase()) && $value != "") {
+          let htmlMovie = d.createElement("li");
+          htmlMovie.innerHTML = MOVIE;
+          $listaDePeliculas.appendChild(htmlMovie);
+        }
+      }
+    });
+  } else {
+    $lupaInput.style.visibility = "hidden";
+    $lupaInput.style.opacity = 0;
+    $searchListContainer.style.display = "none";
+  }
+});
